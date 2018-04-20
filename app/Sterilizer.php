9bb60c4f727361a25c7e684257f80fe7e99d0b9f
kaziahmed91 <kaziahmed91@gmail.com>
2018-04-19 21:00:56 -0400

    adding issues

--- app/Sterilizer.php
+++ app/Sterilizer.php
@@ -14,7 +14,7 @@ class Sterilizer extends Model
     protected $guarded = ['company_id'];
     protected $fillable = 
         ['company_id','sterilizer_name','serial','added_by', 
-        'created_at','manufacturer', 'date_deleted', 'active'];
+        'created_at','manufacturer', 'date_deleted', 'active', 'cycle_number'];
     
     // use SoftDeletes;
     public $timestamps = false;

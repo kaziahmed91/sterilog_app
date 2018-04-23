d72db88826b31565ff06907c2b692188e28aed76
Kazi Ahmed <kaziahmed91@gmail.com>
2018-04-23 15:26:01 -0400

    adding changes to model

--- database/migrations/2018_04_23_165100_add_default_pritner.php
+++ database/migrations/2018_04_23_165100_add_default_pritner.php
@@ -13,7 +13,7 @@ class AddDefaultPritner extends Migration
      */
     public function up()
     {
-        Schema::table('Users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->string('current_printer')->nullable();
         });
     }
